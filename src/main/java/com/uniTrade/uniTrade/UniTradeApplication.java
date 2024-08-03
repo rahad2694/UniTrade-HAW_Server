223d7f9bb8a3b49d4375e2a0c0dbd10005669ce0
Saiful Islam Rahad <rahad.niter@gmail.com>
2024-08-03 19:34:48 +0200

    first commit

---
+++ src/main/java/com/uniTrade/uniTrade/UniTradeApplication.java
@@ -0,0 +1,13 @@
+package com.uniTrade.uniTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTradeApplication.class, args);
+	}
+
+}
