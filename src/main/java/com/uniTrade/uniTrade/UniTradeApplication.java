4a2cef253c42e05cb9a101bcf3a29d77dfb26ad4
Saiful Islam Rahad <rahad.niter@gmail.com>
2024-08-31 22:14:27 +0200

    KAN13: Latest 19 docker modified

--- src/main/java/com/uniTrade/uniTrade/UniTradeApplication.java
+++ src/main/java/com/uniTrade/uniTrade/UniTradeApplication.java
@@ -2,18 +2,19 @@ package com.uniTrade.uniTrade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class UniTradeApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
+		// Dotenv dotenv = Dotenv.load();
 
 		// Set system properties from .env file
-		System.setProperty("SPRING_DATA_MONGODB_URI", dotenv.get("SPRING_DATA_MONGODB_URI"));
-		System.setProperty("SPRING_DATA_MONGODB_DATABASE", dotenv.get("SPRING_DATA_MONGODB_DATABASE"));
+		System.setProperty("SPRING_DATA_MONGODB_URI",
+				"mongodb+srv://asibul279:uniTrade@unitrade.zjgqj.mongodb.net/?retryWrites=true&w=majority&appName=UniTrade");
+		System.setProperty("SPRING_DATA_MONGODB_DATABASE", "UniTrade");
 
 		SpringApplication.run(UniTradeApplication.class, args);
 	}
