fa2ce8464cbed444a9dac3409a3726703f5a5a28
Saiful Islam Rahad <rahad.niter@gmail.com>
2024-09-05 23:05:52 +0200

    KAN-13: disabled auth temporarily

--- src/main/java/com/uniTrade/uniTrade/SecurityConfig.java
+++ src/main/java/com/uniTrade/uniTrade/SecurityConfig.java
@@ -10,17 +10,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()).csrf((csrf) -> csrf.disable())
-                .httpBasic(withDefaults());
-        // http.sessionManagement((session) ->
-        // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        // http.formLogin(withDefaults());
-        // http.httpBasic(withDefaults());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+                .csrf().disable();
+
         return http.build();
     }
-
 }
