83192de9d05d7bb73f51e14e4373b1f7a502af61
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2024-09-07 14:43:35 +0200

    No change

--- src/main/java/com/uniTrade/uniTrade/SecurityConfig.java
+++ src/main/java/com/uniTrade/uniTrade/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         //http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        //http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.formLogin(withDefaults());
+        //http.httpBasic(withDefaults());
         return http.build();
     }
 
